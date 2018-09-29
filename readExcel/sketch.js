75ed65a8194ef98102f83dd2809ffc79eccf0dbc
Ad <thenewadeel@gmail.com>
2018-09-29 14:01:15 +0500

    added sample projs

---
+++ readExcel/sketch.js
@@ -0,0 +1,13 @@
+var data;
+function preload(){
+  data = loadTable("data.csv","header");
+}
+function setup() {
+  // put setup code here
+  console.log(data);
+  createDiv('asdasdasd')
+}
+
+function draw() {
+  // put drawing code here
+}
