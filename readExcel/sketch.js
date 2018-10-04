a3db79ba5c3794e9f6735c5a4652424eed751622
Ad <thenewadeel@gmail.com>
2018-10-04 19:21:11 +0500

    faled in tabulator csv

--- readExcel/sketch.js
+++ readExcel/sketch.js
@@ -1,15 +1,39 @@
 /// <reference path="../p5.global-mode.d.ts" />
 
-var myData,myRows;
-
+var myData, myRows, myTable;
+var myColumns;
 function preload() {
-  myData = loadTable("data.csv", "header");
+  myData = = loadTable("data.csv", "header")
+  // while(myData===undefined){}
+  // console.log('dat now?', await loadTable("data.csv", "header"));
 }
+
+tableSetup = function (rawTableData) {
+  console.log('tableSetup Received:', rawTableData)
+  myTable = new Tabulator("#baseTable", {
+    columns: rawTableData.columns.map(x => {
+      // console.log('rawElem', x)
+      return {
+        "title": x,
+        "field": x,
+        "sorter": "string",
+        "width": 200,
+        "editor": true,
+        "align": "right",
+        "formatter": "progress",
+        "cellClick": function (e, cell) {
+          console.log("cell click", cell)
+        },
+      }
+    })
+  });
+  myTable.setData(myData);
+};
 
 function setup() {
   // put setup code here
   // console.log(data);
-  setupTable(myData);
+  tableSetup(myData)
   noLoop();
 }
 
